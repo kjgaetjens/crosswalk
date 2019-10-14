933344a33a82fc07a8d7d7a67bca913905f75b1d
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-10-14 18:35:39 -0500

    set up build for deployment

---
+++ client-admin/build/precache-manifest.98fbb12e927e471a9f15ade8ad2b6795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8b6908ec6fff8899ca5998482eb2a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe9c56fdefc2118c977a",
+    "url": "/static/css/main.800f161d.chunk.css"
+  },
+  {
+    "revision": "3dc62f1c529214f050d7",
+    "url": "/static/js/2.dc8f54cd.chunk.js"
+  },
+  {
+    "revision": "fe9c56fdefc2118c977a",
+    "url": "/static/js/main.2f699e39.chunk.js"
+  },
+  {
+    "revision": "5d1f97e5017119fa2e3c",
+    "url": "/static/js/runtime-main.52b75379.js"
+  },
+  {
+    "revision": "e9d0189fdbad7e3b5b46afcbe72ff832",
+    "url": "/static/media/legendContinuous.e9d0189f.png"
+  }
+]);
