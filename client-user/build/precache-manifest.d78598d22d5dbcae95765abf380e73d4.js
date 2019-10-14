22c46832d2a59cde4719f02b33349e1ef743262e
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-10-14 17:36:59 -0500

    updated redirects

---
+++ client-user/build/precache-manifest.d78598d22d5dbcae95765abf380e73d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583c173ace046daaea4b826a5eeaa6a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64652529e6c0fdc4bfdb",
+    "url": "/static/css/main.ac0aedb2.chunk.css"
+  },
+  {
+    "revision": "6df34794fdad6ba42585",
+    "url": "/static/js/2.372628ef.chunk.js"
+  },
+  {
+    "revision": "64652529e6c0fdc4bfdb",
+    "url": "/static/js/main.8e5bbda1.chunk.js"
+  },
+  {
+    "revision": "bc210b20960983021173",
+    "url": "/static/js/runtime-main.27745bea.js"
+  }
+]);
